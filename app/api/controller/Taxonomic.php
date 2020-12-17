383004d6deb2a5cccfd16d42a549937b6b07d0cd
杨航 <hang.yang@jibaoyuan.net>
2020-12-18 00:53:56 +0800

    类型修改

--- app/api/controller/Taxonomic.php
+++ app/api/controller/Taxonomic.php
@@ -16,9 +16,9 @@ class Taxonomic extends BaseController
     public function items($id){
         $taxonomic = new ModelTaxonomic();
         $data['info'] = (new \app\api\model\Primary())->where('id',$id)->find();
-        $data['items'] = $taxonomic->where('parentid',$id)->limit(20)->field('id,parentid,name')->with(['navtag'=>function($query){
-            $query->withLimit(5)->order('id desc')->field('id,describe,it_name,pic,keywords,create_time,parentid,uid,icon,author');
-        }])->select();
+        $data['items'] = $taxonomic->where('parentid',$id)->field('id,parentid,name')->with(['navtag'=>function($query){
+            $query->withLimit(3)->order('id desc')->field('id,describe,it_name,pic,keywords,create_time,parentid,uid,icon,author');
+        }])->limit(12)->select();
         return $this->showWebData(['data'=>$data]);
     }
 
--- app/api/controller/Wxcontent.php
+++ app/api/controller/Wxcontent.php
@@ -45,18 +45,14 @@ class Wxcontent extends BaseController
         $data['CAT'] = $model->where('ArticleType','CAT')->limit(12)->field('ArticleTitle,ArticleType,ArticlePublishDateTime,id')->order('id desc')->cache($time)->select();
         $data['GAME'] = $model->where('ArticleType','GAME')->limit(12)->field('ArticleTitle,ArticleType,ArticlePublishDateTime,id')->order('id desc')->cache($time)->select();
         $data['MILITARY'] = $model->where('ArticleType','MILITARY')->limit(12)->field('ArticleTitle,ArticleType,ArticlePublishDateTime,id')->order('id desc')->cache($time)->select();
-        $data['SPORTS'] = $model->where('ArticleType','SPORTS')->limit(12)->field('ArticleTitle,ArticleType,ArticlePublishDateTime,id')->order('id desc')->cache($time)->cache($time)->select();
-        $data['HOROSCOPE'] = $model->where('ArticleType','HOROSCOPE')->limit(12)->field('ArticleTitle,ArticleType,ArticlePublishDateTime,id')->order('id desc')->cache($time)->select();
-        $data['EDUCATION'] = $model->where('ArticleType','EDUCATION')->limit(12)->field('ArticleTitle,ArticleType,ArticlePublishDateTime,id')->order('id desc')->cache($time)->select();
-        $data['HISTORY'] = $model->where('ArticleType','HISTORY')->limit(12)->field('ArticleTitle,ArticleType,ArticlePublishDateTime,id')->order('id desc')->cache($time)->select();
-        $data['WORK'] = $model->where('ArticleType','WORK')->limit(12)->field('ArticleTitle,ArticleType,ArticlePublishDateTime,id')->order('id desc')->cache($time)->select();
-        $data['TRAVEL'] = $model->where('ArticleType','TRAVEL')->limit(12)->field('ArticleTitle,ArticleType,ArticlePublishDateTime,id')->order('id desc')->cache($time)->select();
-        $data['FASHION'] = $model->where('ArticleType','FASHION')->limit(12)->field('ArticleTitle,ArticleType,ArticlePublishDateTime,id')->order('id desc')->cache($time)->select();
-        $data['LIFE'] = $model->where('ArticleType','LIFE')->limit(12)->field('ArticleTitle,ArticleType,ArticlePublishDateTime,id')->order('id desc')->cache($time)->select();
-        $data['CAR'] = $model->where('ArticleType','CAR')->limit(12)->field('ArticleTitle,ArticleType,ArticlePublishDateTime,id')->order('id desc')->cache($time)->select();
-        $data['FINANCE'] = $model->where('ArticleType','FINANCE')->limit(12)->field('ArticleTitle,ArticleType,ArticlePublishDateTime,id')->order('id desc')->cache($time)->select();
-        $data['TECH'] = $model->where('ArticleType','TECH')->limit(12)->field('ArticleTitle,ArticleType,ArticlePublishDateTime,id')->order('id desc')->cache($time)->select();
-        $data['FUNNY'] = $model->where('ArticleType','FUNNY')->limit(12)->field('ArticleTitle,ArticleType,ArticlePublishDateTime,id')->order('id desc')->cache($time)->select();
+//        $data['SPORTS'] = $model->where('ArticleType','SPORTS')->limit(12)->field('ArticleTitle,ArticleType,ArticlePublishDateTime,id')->order('id desc')->cache($time)->cache($time)->select();
+//        $data['EDUCATION'] = $model->where('ArticleType','EDUCATION')->limit(12)->field('ArticleTitle,ArticleType,ArticlePublishDateTime,id')->order('id desc')->cache($time)->select();
+//        $data['HISTORY'] = $model->where('ArticleType','HISTORY')->limit(12)->field('ArticleTitle,ArticleType,ArticlePublishDateTime,id')->order('id desc')->cache($time)->select();
+//        $data['WORK'] = $model->where('ArticleType','WORK')->limit(12)->field('ArticleTitle,ArticleType,ArticlePublishDateTime,id')->order('id desc')->cache($time)->select();
+//        $data['TRAVEL'] = $model->where('ArticleType','TRAVEL')->limit(12)->field('ArticleTitle,ArticleType,ArticlePublishDateTime,id')->order('id desc')->cache($time)->select();
+//        $data['FASHION'] = $model->where('ArticleType','FASHION')->limit(12)->field('ArticleTitle,ArticleType,ArticlePublishDateTime,id')->order('id desc')->cache($time)->select();
+//        $data['TECH'] = $model->where('ArticleType','TECH')->limit(12)->field('ArticleTitle,ArticleType,ArticlePublishDateTime,id')->order('id desc')->cache($time)->select();
+//        $data['FUNNY'] = $model->where('ArticleType','FUNNY')->limit(12)->field('ArticleTitle,ArticleType,ArticlePublishDateTime,id')->order('id desc')->cache($time)->select();
         return $this->showWebData(['data'=>$data]);
     }
 
