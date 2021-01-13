793cb70421829c8a868c86d16f13751b32ffed42
杨航 <hang.yang@jibaoyuan.net>
2021-01-13 23:45:45 +0800

    删除大数据

--- app/api/controller/Wxcontent.php
+++ app/api/controller/Wxcontent.php
@@ -45,14 +45,6 @@ class Wxcontent extends BaseController
         $data['CAT'] = $model->where('ArticleType','CAT')->limit(12)->field('ArticleTitle,ArticleType,ArticlePublishDateTime,id')->order('id desc')->cache($time)->select();
         $data['GAME'] = $model->where('ArticleType','GAME')->limit(12)->field('ArticleTitle,ArticleType,ArticlePublishDateTime,id')->order('id desc')->cache($time)->select();
         $data['MILITARY'] = $model->where('ArticleType','MILITARY')->limit(12)->field('ArticleTitle,ArticleType,ArticlePublishDateTime,id')->order('id desc')->cache($time)->select();
-//        $data['SPORTS'] = $model->where('ArticleType','SPORTS')->limit(12)->field('ArticleTitle,ArticleType,ArticlePublishDateTime,id')->order('id desc')->cache($time)->cache($time)->select();
-//        $data['EDUCATION'] = $model->where('ArticleType','EDUCATION')->limit(12)->field('ArticleTitle,ArticleType,ArticlePublishDateTime,id')->order('id desc')->cache($time)->select();
-//        $data['HISTORY'] = $model->where('ArticleType','HISTORY')->limit(12)->field('ArticleTitle,ArticleType,ArticlePublishDateTime,id')->order('id desc')->cache($time)->select();
-//        $data['WORK'] = $model->where('ArticleType','WORK')->limit(12)->field('ArticleTitle,ArticleType,ArticlePublishDateTime,id')->order('id desc')->cache($time)->select();
-//        $data['TRAVEL'] = $model->where('ArticleType','TRAVEL')->limit(12)->field('ArticleTitle,ArticleType,ArticlePublishDateTime,id')->order('id desc')->cache($time)->select();
-//        $data['FASHION'] = $model->where('ArticleType','FASHION')->limit(12)->field('ArticleTitle,ArticleType,ArticlePublishDateTime,id')->order('id desc')->cache($time)->select();
-//        $data['TECH'] = $model->where('ArticleType','TECH')->limit(12)->field('ArticleTitle,ArticleType,ArticlePublishDateTime,id')->order('id desc')->cache($time)->select();
-//        $data['FUNNY'] = $model->where('ArticleType','FUNNY')->limit(12)->field('ArticleTitle,ArticleType,ArticlePublishDateTime,id')->order('id desc')->cache($time)->select();
         return $this->showWebData(['data'=>$data]);
     }
 
